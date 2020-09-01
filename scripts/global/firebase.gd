ef001e6af236fedad60c9e99c1d06c8a4ea76f92
Surge <esfloyd9341@gmail.com>
2020-09-01 15:16:14 -0400

    Begin adding work for user logins

--- scripts/global/firebase.gd
+++ scripts/global/firebase.gd
@@ -2,4 +2,41 @@ extends Node
 
 const apikey = 'AIzaSyDrkZuYjzbynZNE5KfL5H0_TLUSAen0mQs'
 
-const rgstr = ''
+const rgstr = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + apikey
+const login = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + apikey
+
+var uauthtoken: String
+
+signal doneLoggingIn(error)
+signal doneRegistering(error)
+
+func login(email, password, http: HTTPRequest):
+	var body = {
+		email: email,
+		password: password
+	}
+	http.request(rgstr,[],false,HTTPClient.METHOD_POST,to_json(body))
+	var result = yield(http, "request_completed") as Array
+	print(result[1])
+	if result[1] == 200:
+		uauthtoken = JSON.parse(result[3].get_string_from_ascii()).result.idtoken
+		logcat.stdout("Successfully registered user: " + uauthtoken, logcat.INFO)
+		emit_signal("doneLoggingIn", null)
+	else:
+		emit_signal("doneLoggingIn", "An error has occoured.")
+	#TODO: error handling
+
+func register(email, password, http: HTTPRequest):
+	var body = {
+		email: email,
+		password: password
+	}
+	http.request(login,[],false,HTTPClient.METHOD_POST,to_json(body))
+	var result = yield(http, "request_completed") as Array
+	print(result[1])
+	if result[1] == 200:
+		uauthtoken = JSON.parse(result[3].get_string_from_ascii()).result.idtoken
+		logcat.stdout("Successfully logged in user: " + uauthtoken, logcat.INFO)
+		emit_signal("doneRegistering", null)
+	else:
+		emit_signal("doneRegistering", "An error has occoured.")
--- scripts/useracct.gd
+++ scripts/useracct.gd
@@ -1,10 +1,37 @@
 extends Control
 
 func _ready():
-	pass
+	$"Panel/TabContainer/Log In/Panel".visible = false
+	var e = get_tree().get_root().get_node("fb").connect("doneLoggingIn",self,"loginFinished")
+	var a = get_tree().get_root().get_node("fb").connect("doneRegistering",self,"registerFinished")
+	if e:
+		print("LOGIN FINISH CONNECTION ERROR")
+	if a:
+		print("REGISTER FINISH CONNECTION ERROR")
+	$"Panel/TabContainer/Log In/error".text = ""
+	$"Panel/TabContainer/Sign up/Label".text = ""
 
-func _login():
-	pass
+func loginPressed():
+	logcat.stdout("Attempting client login...", logcat.INFO)
+	$"Panel/TabContainer/Log In/Panel".visible = true
+	fb.login($"Panel/TabContainer/Log In/email".get_text(), $"Panel/TabContainer/Log In/pass".get_text(), $HTTPRequest)
 
-func _register():
-	pass
+func registerPressed():
+	if $"Panel/TabContainer/Sign up/pass".get_text() == $"Panel/TabContainer/Sign up/passc".get_text():
+		logcat.stdout("Attempting register client of user: " + $"Panel/TabContainer/Sign up/uname".get_text(), logcat.INFO)
+		$"Panel/TabContainer/Sign up/Panel".visible = true
+		fb.register($"Panel/TabContainer/Sign up/email".get_text(), $"Panel/TabContainer/Sign up/pass".get_text(), $HTTPRequest)
+
+func loginFinished(error):
+	if !error:
+		$"Panel/TabContainer/Log In/Panel".visible = false
+	else:
+		$"Panel/TabContainer/Log In/error".text = error
+		$"Panel/TabContainer/Log In/Panel".visible = false
+
+func registerFinished(error):
+	if !error:
+		$"Panel/TabContainer/Sign Up/Panel".visible = false
+	else:
+		$"Panel/TabContainer/Sign up/Label".text = error
+		$"Panel/TabContainer/Sign up/Panel".visible = false

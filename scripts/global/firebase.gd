39195309210f2d56f5d208d2ad3d45cb8221132f
Animus-Surge <esfloyd9341@gmail.com>
2020-09-01 12:34:07 -0400

    backup

---
+++ scripts/global/firebase.gd
@@ -0,0 +1,5 @@
+extends Node
+
+const apikey = 'AIzaSyDrkZuYjzbynZNE5KfL5H0_TLUSAen0mQs'
+
+const rgstr = ''
---
+++ scripts/useracct.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	pass
+
+func _login():
+	pass
+
+func _register():
+	pass

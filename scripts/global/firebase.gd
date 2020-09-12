b9e249bf08927a628e6f82d9a52d4c22535c4314
Surge <esfloyd9341@gmail.com>
2020-09-12 18:59:24 -0400

    Revamp login system

--- scripts/global/firebase.gd
+++ scripts/global/firebase.gd
@@ -1,71 +1,8 @@
 extends Node
 
-const apikey = 'AIzaSyDrkZuYjzbynZNE5KfL5H0_TLUSAen0mQs'
+const APIKEY = "AIzaSyDrkZuYjzbynZNE5KfL5H0_TLUSAen0mQs"
 
-const rgstr = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + apikey
-const login = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + apikey
-const profileUpdate = 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=' + apikey
-const lookup = 'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=' + apikey
+#authentication URLS
+const AuthSI = ""
+const authSU = ""
 
-onready var http = get_tree().get_root().get_node("http")
-
-var uauthtoken: String
-var username: String
-
-signal doneLoggingIn(error)
-signal doneRegistering(error)
-
-func login(email, password):
-	var body = {
-		"email": email,
-		"password": password
-	}
-	http.request(login,[],false,HTTPClient.METHOD_POST,to_json(body))
-	var result = yield(http, "request_completed") as Array
-	if result[1] == 200:
-		uauthtoken = JSON.parse(result[3].get_string_from_ascii()).result.idToken
-		#logcat.stdout("Successfully logged in user: " + uauthtoken, logcat.INFO)
-		getUname()
-	else:
-		emit_signal("doneLoggingIn", "An error has occoured: " + String(result[1]))
-	#TODO: error handling
-
-func register(email, password, uname):
-	var body = {
-		"email": email,
-		"password": password
-	}
-	http.request(rgstr,[],false,HTTPClient.METHOD_POST,to_json(body))
-	var result = yield(http, "request_completed") as Array
-	if result[1] == 200:
-		uauthtoken = JSON.parse(result[3].get_string_from_ascii()).result.idToken
-		#logcat.stdout("Successfully registered user: " + uauthtoken, logcat.INFO)
-		updateProfile(uname)
-	else:
-		emit_signal("doneRegistering", "An error has occoured: " + String(result[1]))
-
-func updateProfile(dname):
-	var body = {
-		"idToken":uauthtoken,
-		"displayName":dname
-	}
-	http.request(profileUpdate,[],false,HTTPClient.METHOD_POST,to_json(body))
-	var result = yield(http, "request_completed") as Array
-	if !result[1] == 200:
-		emit_signal("doneRegistering", "An error has occoured while setting username. " + String(result[1]))
-		return
-	username = JSON.parse(result[3].get_string_from_ascii()).result.displayName
-	emit_signal("doneRegistering", null)
-
-func getUname():
-	var body = {
-		"idToken": uauthtoken
-	}
-	http.request(lookup,[],false,HTTPClient.METHOD_POST,to_json(body))
-	var result = yield(http, "request_completed") as Array
-	#print(JSON.parse(result[3].get_string_from_ascii()).result)
-	if !result[1] == 200:
-		emit_signal("doneLoggingIn", "An error has occoured while getting your username. " + String(result[1]))
-		return
-	username = JSON.parse(result[3].get_string_from_ascii()).result.users[0].displayName
-	emit_signal("doneLoggingIn", null)
--- scripts/useracct.gd
+++ scripts/useracct.gd
@@ -1,76 +1,16 @@
 extends Control
 
-onready var lipanel = get_node("Panel/TabContainer/Log In/Panel")
-onready var supanel = get_node("Panel/TabContainer/Sign up/Panel")
-
-onready var lierror = get_node("Panel/TabContainer/Log In/error")
-onready var suerror = get_node("Panel/TabContainer/Sign up/Label")
-
-onready var spass = get_node("Panel/TabContainer/Sign up/pass")
-onready var spassc = get_node("Panel/TabContainer/Sign up/passc")
-onready var semail = get_node("Panel/TabContainer/Sign up/email")
-onready var suname = get_node("Panel/TabContainer/Sign up/uname")
-
-onready var lpass = get_node("Panel/TabContainer/Log In/pass")
-onready var lemail= get_node("Panel/TabContainer/Log In/email")
-
 func _ready():
-	lipanel.visible = false
-	var e = get_tree().get_root().get_node("fb").connect("doneLoggingIn",self,"loginFinished")
-	var a = get_tree().get_root().get_node("fb").connect("doneRegistering",self,"registerFinished")
-	if e:
-		print("LOGIN FINISH CONNECTION ERROR")
-	if a:
-		print("REGISTER FINISH CONNECTION ERROR")
-	lierror.text = ""
-	suerror.text = ""
-	
-	globalvars.setCurrentScene("useracct")
+	pass
 
-func singleplayerpressed():
-	scenes.load_scene("res://scenes/menus.tscn")
+func showLogin():
+	$AnimationPlayer.play("lipanel")
 
-func loginPressed():
-	logcat.stdout("Attempting client login...", logcat.INFO)
-	lipanel.visible = true
-	fb.login(lemail.get_text(), lpass.get_text())
+func hideLogin():
+	pass
 
-func registerPressed():
-	if spass.get_text() == spassc.get_text():
-		logcat.stdout("Attempting register client of user: " + suname.get_text(), logcat.INFO)
-		supanel.visible = true
-		fb.register(semail.get_text(), spass.get_text(), suname.get_text())
+func showSignup():
+	$AnimationPlayer.play("supanel")
 
-func loginFinished(error):
-	if !error:
-		checkLogin("login")
-		logcat.stdout("Successfully logged in user: " + fb.username, logcat.INFO)
-		lipanel.visible = false
-		globalvars.loggedIn = true
-		scenes.load_scene("res://scenes/menus.tscn")
-	else:
-		lierror.text = error
-		lipanel.visible = false
-
-func registerFinished(error):
-	if !error:
-		checkLogin("register")
-		logcat.stdout("Successfully registered user with username: " + fb.username, logcat.INFO)
-		supanel.visible = false
-		globalvars.loggedIn = true
-		scenes.load_scene("res://scenes/menus.tscn")
-	else:
-		suerror.text = error
-		supanel.visible = false
-
-func checkLogin(side):
-	if $"Panel/TabContainer/Log In/CheckBox".pressed or $"Panel/TabContainer/Sign up/CheckBox".pressed:
-		var cfgFile = File.new()
-		cfgFile.open_encrypted_with_pass("user://login.ids", File.WRITE, OS.get_unique_id())
-		var details
-		if side == "register":
-			details = semail + " " + spass
-		elif side == "login":
-			details = lemail + " " + lpass
-		cfgFile.store_line(details)
-		cfgFile.close()
+func hideSignup():
+	pass

34e43ce7bfff4ca7cb84815027d5511d2bd76455
Surge <esfloyd9341@gmail.com>
2020-09-21 07:43:17 -0400

    Remove unneccessary files and files to be redesigned

--- scripts/character.gd
+++ scripts/character.gd
@@ -12,7 +12,7 @@ onready var headpalette = load("res://images/character/palettes/head-palette.png
 onready var scalepalette = load("res://images/character/palettes/body_leg_tail-palette.png")
 onready var wingpalette = load("res://images/character/palettes/wing-palette.png")
 
-#TODO: have it load from previously used slot (FileManager TODO) and have the four character slots
+var previousSlot #TODO
 
 func _ready():
 	pass
@@ -31,6 +31,16 @@ func wingsChangeC(color):
 
 func spineChangeC(color):
 	pass
+
+#tribes
+#iw - 0
+#mw - 1
+#nw - 2
+#rw - 3
+#saw - 4
+#sew - 5
+#skw - 6
+#...
 
 func partChange(part, tribe):
 	pass

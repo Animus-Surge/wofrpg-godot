f1e3d06b3eee2f129411fb651bbbb69491dd3c21
Surge <esfloyd9341@gmail.com>
2020-10-28 23:25:14 -0400

    Implement firebase auth system in launcher

---
+++ wofrpg-launcher-NEW/firebase.gd
@@ -0,0 +1,83 @@
+extends Node
+
+signal errored(type, id)
+signal success(type)
+
+var idToken
+
+const WEBAPIKEY = "AIzaSyDrkZuYjzbynZNE5KfL5H0_TLUSAen0mQs"
+
+const URL_SIGNUP = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + WEBAPIKEY
+const URL_SIGNIN = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + WEBAPIKEY
+const URL_AUTDEL = "https://identitytoolkit.googleapis.com/v1/accounts:delete?key=" + WEBAPIKEY
+
+const URL_DBFORE = "https://wofrpg-main.firebaseio.com/"
+
+onready var http = get_parent().get_node("HTTPRequest")
+
+func signUp(uname:String, email:String, password:String): #Password checking will happen in signup script
+	http.request(URL_DBFORE + "users/" + uname + ".json")
+	var result = yield(http, "request_completed") as Array
+	if result[1] == 200:
+		if result[3].get_string_from_ascii() != "null":
+			emit_signal("errored", "Username already exists", "ERR_UNAME_EXISTS")
+			return
+		var reqbody = {
+			"email":email.strip_edges(),
+			"password":password.strip_edges()
+		}
+		http.request(URL_SIGNUP, [], false, HTTPClient.METHOD_POST, to_json(reqbody))
+		result = yield(http, "request_completed") as Array
+		if result[1] != 200:
+			var error = JSON.parse(result[3].get_string_from_ascii()).result
+			print(error.error)
+			match error.error.errors[0].message:
+				"INVALID_EMAIL":
+					emit_signal("errored", "Email was invalid", "ERR_EMAIL_INVALID")
+				"EMAIL_EXISTS":
+					emit_signal("errored", "Email already exists", "ERR_EMAIL_EXISTS")
+			return
+		idToken = JSON.parse(result[3].get_string_from_ascii()).result.idToken
+		reqbody = {
+			"displayname":uname,
+			"email":email,
+			"verified":false #Will be worked with in the future
+		}
+		http.request(URL_DBFORE + "users/" + uname + ".json?access_token=" + idToken, [], false, HTTPClient.METHOD_PUT, to_json(reqbody))
+		result = yield(http, "request_completed") as Array
+		
+		if result[1] != 200:
+			emit_signal("errored", "Database pushing failed.", "ERR_HTTP-PUT_FAILURE")
+			reqbody = {
+				"idToken":idToken
+			}
+			http.request(URL_AUTDEL, [], false, HTTPClient.METHOD_POST, to_json(reqbody))
+			result = yield(http, "request_completed") as Array
+			return
+		emit_signal("success", "USER_SIGNUP")
+	else:
+		emit_signal("errored", "Error reading from database", "ERR_HTTP-GET_FAILURE")
+
+func signIn(uname, password):
+	http.request(URL_DBFORE + "users/" + uname + ".json")
+	var result = yield(http, "request_completed") as Array
+	if result[1] == 200:
+		var out = result[3].get_string_from_ascii()
+		if out == "null":
+			emit_signal("errored", "Username not found", "ERR_UNKNOWN_USERNAME")
+			return
+		var reqbody = {
+			"email":JSON.parse(out).result.email,
+			"password":password
+		}
+		http.request(URL_SIGNIN, [], false, HTTPClient.METHOD_POST, to_json(reqbody))
+		result = yield(http, "request_completed") as Array
+		if result[1] == 200:
+			out = JSON.parse(result[3].get_string_from_ascii()).result
+			idToken = out.idToken
+			emit_signal("success", "USER_LOGIN")
+		else:
+			print(result[3].get_string_from_ascii())
+	else:
+		emit_signal("errored", "Error reading from database", "ERR_HTTP-GET_FAILURE")
+	
--- wofrpg-launcher-NEW/systemroot.gd
+++ wofrpg-launcher-NEW/systemroot.gd
@@ -5,5 +5,102 @@ var loggedIn = false
 func _ready():
 	pass
 
-func onSidebar(): #redirected from Panel/Button:_pressed()
+##########################
+# Callbacks from buttons #
+##########################
+
+func onSidebarShowPressed():
+	$AnimationPlayer.play("sidebar-showhide")
+func onSidebarHidePressed():
+	$AnimationPlayer.play_backwards("sidebar-showhide")
+
+##################
+# SU/LI handlers #
+##################
+
+func suVisChanged():
+	$signup/email.modulate = Color.white
+	$signup/uname.modulate = Color.white
+	$signup/password.modulate = Color.white
+	$signup/passwordc.modulate = Color.white
+	$signup/errorlabel.text = ""
+
+func siVisChanged():
+	$signin/uname.modulate = Color.white
+	$signin/password.modulate = Color.white
+	$signin/errorlabel.text = ""
+
+func signupPressed():
+	suVisChanged()
+	if $signup/uname.text == "":
+		$signup/uname.modulate = Color.red
+		$signup/errorlabel.text = "Username cannot be blank"
+		return
+	var pw = get_node("signup/password").text
+	var pwc = get_node("signup/passwordc").text
+	if pw != pwc:
+		$signup/errorlabel.text = "Passwords don't match"
+		$signup/password.modulate = Color.red
+		$signup/passwordc.modulate = Color.red
+		return
+	if pw == "":
+		$signup/password.modulate = Color.red
+		$signup/errorlabel.text = "Password cannot be blank"
+		return
+	if pwc == "":
+		$signup/passwordc.modulate = Color.red
+		$signup/errorlabel.text = "Password Confirm cannot be blank"
+		return
+	if !isValidEmail($signup/email.text):
+		$signup/email.modulate = Color.red
+		return
+	$firebase.signUp($signup/uname.text, $signup/email.text, pw)
+
+func loginPressed():
+	siVisChanged()
+	var uname = $signin/uname.text
+	var pw = $signin/password.text
+	
+	if uname == "":
+		$signin/errorlabel.text = "Username cannot be blank"
+		$signin/uname.modulate = Color.red
+	if pw == "":
+		$signin/errorlabel.text = "Password cannot be blank"
+		$signin/password.modulate = Color.red
+	
+	$firebase.signIn(uname, pw)
+
+func forgotPassword(): #TODO
 	pass
+
+func errored(type, id):
+	match id:
+		"ERR_EMAIL_INVALID":
+			$signup/errorlabel.text = type
+			$signup/email.modulate = Color.red
+		"ERR_EMAIL_EXISTS":
+			$signup/errorlabel.text = type
+			$signup/email.modulate = Color.red
+		"ERR_HTTP-PUT_FAILURE":
+			$signup/errorlabel.text = type
+			$signin/errorlabel.text = type
+		"ERR_PASSWORD_INVALID":
+			$signup/password.modulate = Color.red
+			$signup/errorlabel.text = type
+		"ERR_UNKNOWN_USERNAME":
+			$signin/errorlabel.text = type
+			$signin/uname.modulate = Color.red
+
+func rememberToggle(_button_pressed):
+	pass #TODO
+
+##################
+# Misc functions #
+##################
+
+func isValidEmail(email:String) -> bool:
+	if email.find("@") == -1:
+		return false
+	if email.find(".com") == -1 and email.find(".net") == -1 and email.find(".org") == -1 and email.find(".edu") == -1:
+		return false
+	return true
